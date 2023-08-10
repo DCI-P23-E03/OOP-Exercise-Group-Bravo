7eacada0a59d48706ee5c387d2f551f84e993ca6
Udokaamos <udokaamos@gmail.com>
2023-08-10 15:05:26 +0200

    solution for oop

---
+++ solution.py
@@ -0,0 +1,26 @@
+class User:
+    
+    # first the constructor
+    def __init__(self, username, password, email, new_password):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.new_password = new_password
+
+    # instance method
+    def register_user(self):
+        print(f"You have registered {self.username} with password: {self.password} and email address: {self.email}")
+
+    # instance method
+    def login_user(self):
+        print(f"Congratulations {self.username}!!! Your login with the password: {self.password} was successful")
+
+    # instance method
+    def change_password(self):
+        print(f"Your old password: {self.password} has been changed to {self.new_password}")
+
+new_password = input("Enter your new password: ")
+user_info = User("john123", "pass123", "john@example.com", new_password)
+user_info.register_user()
+user_info.login_user()
+user_info.change_password()
